dc6fdcc3bf985085710767d9ba33c746c54b4a2c
alexaleku@gmail.com <alexaleku@gmail.com>
2022-07-25 19:38:59 -0700

    Pagination APIs project with tests formatted ready

--- src/PaginationHelper.java
+++ src/PaginationHelper.java
@@ -1,4 +1,102 @@
+/**
+ * PaginationHelper class used to implement UI-like pagination functionality on java API level
+ */
 public class PaginationHelper {
 
 
+    private final Object[] values;
+    private final int countPerPage;
+
+    /**
+     * @param values       An array of values that contain objects of any type, should be non-empty
+     * @param countPerPage Defines how many items per page should be shown, should be > 0
+     * @throws IllegalArgumentException when initial parameters don't meet the outlined requirements
+     */
+    public PaginationHelper(Object[] values, int countPerPage) throws IllegalArgumentException {
+        if (countPerPage <= 0) {
+            throw new IllegalArgumentException("Count per page is invalid, should be greater then 0");
+        }
+        if (values.length == 0) {
+            throw new IllegalArgumentException("Empty array, please add some items");
+        }
+        this.values = values;
+        this.countPerPage = countPerPage;
+    }
+
+    /**
+     * @return total number of pages
+     */
+    public int pageCount() {
+        if (isLastPageIncomplete()) {
+            return values.length / countPerPage + 1;
+        } else {
+            return values.length / countPerPage;
+        }
+    }
+
+    /**
+     * @return total number of items
+     */
+    public int itemCount() {
+        return values.length;
+    }
+
+    public int pageItemCount(int pageIndex) {
+        if (pageIndex < 0 || pageIndex > lastPageIndex()) {
+            return -1;
+        } else if (isLastPageIncomplete() && lastPageIndex() == pageIndex) {
+            return values.length % countPerPage;
+        } else {
+            return countPerPage;
+        }
+    }
+
+
+    /**
+     * @param itemIndex item index in the initial array passed as a parameter to the class constructor
+     * @return a page index of the page where the item should be located
+     */
+    public int pageIndex(int itemIndex) {
+        if (itemIndex < 0 || itemIndex >= values.length) {
+            return -1;
+        } else {
+            return itemIndex / countPerPage;
+        }
+    }
+
+
+    /**
+     * @param pageIndex index of the page to get items that this page contains
+     * @return Object[] an array of items that the page contains
+     * @throws IllegalArgumentException thrown when provided page index is out of range
+     */
+    public Object[] getItemsForPageIndex(int pageIndex) throws IllegalArgumentException {
+        if (pageIndex < 0 || pageIndex > lastPageIndex()) {
+            throw new IllegalArgumentException("Page doesn't exist");
+        } else {
+            int pageFirstItemIndex = pageIndex * countPerPage;
+            int pageItemCount = pageItemCount(pageIndex);
+            Object[] pageItems = new Object[pageItemCount];
+            for (int i = 0; i < pageItemCount; i++) {
+                pageItems[i] = values[pageFirstItemIndex + i];
+            }
+            return pageItems;
+        }
+    }
+
+    // Helper methods
+
+    /**
+     * @return return last page index
+     */
+    private int lastPageIndex() {
+        return pageCount() - 1;
+    }
+
+    /**
+     * @return true if last page doesn't have the amount of items that all other pages, false otherwise
+     */
+    private boolean isLastPageIncomplete() {
+        return values.length % countPerPage != 0;
+    }
 }
--- test/PaginationHelperTest.java
+++ test/PaginationHelperTest.java
@@ -1,99 +1,194 @@
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-class PaginationHelperTest {
+/**
+ * Test class that have test methods to verify PaginationHelper class functionality
+ */
+public class PaginationHelperTest {
 
-    Object[] values = new Object[]{'a', 'b', 'c', 'd', 'e', 'f'};
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
-//    @Before
-//    public void setUp() throws Exception {
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        values = null;
-//    }
+    Object[] testValues = new Object[]{'a', 'b', 'c', 'd', 'e', 'f'};
+    Object[] testValuesOneItem = new Object[]{'a'};
 
-
-
-
-    @Test
-    public void testItemsFitToOnePageWhenItemsOnPageCountEqualToNumberOfItems() {
-        Object[] values = new Object[]{};
-        PaginationHelper helper = new PaginationHelper(values, 1);
-    }
-
-    @Test
-    public void testItemsFitToOnePageWhenItemsOnPageCountLessThenNumberOfItems() {
-        Object[] values = new Object[]{};
-        PaginationHelper helper = new PaginationHelper(values, 1);
-    }
-
-    @Test
-    public void testItemsFitToOnePageWhenItemsOnPageCountLessThenNumberOfItems1() {
-        Object[] values = new Object[]{};
-        PaginationHelper helper = new PaginationHelper(values, 1);
-    }
-
-
-    // NEGATIVE TESTING
+    // TESTS FOR ACCEPTED VALUE REQUIREMENTS
     @Test
     public void testEmptyItemsShouldNotBeAccepted() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Empty array, please add some items");
         Object[] values = new Object[]{};
-        PaginationHelper helper = new PaginationHelper(values, 1);
+        new PaginationHelper(values, 1);
     }
 
     @Test
     public void testCountPerPageCantBeLessThenZero() {
-        PaginationHelper helper = new PaginationHelper(values, -1);
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Count per page is invalid, should be greater then 0");
+        new PaginationHelper(testValues, -1);
     }
 
     @Test
     public void testCountPerPageCantBeZero() {
-        PaginationHelper helper = new PaginationHelper(values, 0);
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Count per page is invalid, should be greater then 0");
+        new PaginationHelper(testValues, 0);
     }
 
+    // TESTS FOR THE CLASS API METHODS
 
-
-    @Test
-    public void pageCount() {
-        Object[] values = new Object[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'l', 'n', 'o', 'p'};
-    }
+    // Tests for int pageCount() API
 
     @Test
-    public void itemCount() {
-    }
-
-    @Test
-    public void pageItemCount() {
+    public void testPageCount_SeveralPages() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("There should be 2 pages in 6-item array broken on pages with 4 items count",
+                2, helper.pageCount());
     }
 
     @Test
-    public void pageIndex() {
+    public void testPageCount_OnePage() {
+        PaginationHelper helper = new PaginationHelper(testValuesOneItem, 4);
+        Assert.assertEquals("There should be 1 page in 1 item array broken on pages with 4 items count",
+                1, helper.pageCount());
+    }
+
+    // Tests for int itemCount() API
+
+    @Test
+    public void testItemCount_OneItem() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("There should be 6 items as an array that was used for class setup have 6 item",
+                6, helper.itemCount());
     }
 
     @Test
-    public void getItemsForPageIndex() {
+    public void testItemCount_MoreThenOneItem() {
+        PaginationHelper helper = new PaginationHelper(testValuesOneItem, 4);
+        Assert.assertEquals("There should be 1 item as an array that was used for class setup have 1 item",
+                1, helper.itemCount());
+    }
+
+    // Tests for int pageItemCount(int pageIndex) API
+
+    @Test
+    public void testPageItemCount_FirstPage() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("There should be 4 items on page 0 for 6-item array broken into pages with 4 items",
+                4, helper.pageItemCount(0));
     }
 
     @Test
-    public void testPageCount() {
+    public void testPageItemCount_LastPageWithIncompleteNumberOfItems() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("There should be 2 items on page 1 for 6-item array broken into pages with 4 items",
+                2, helper.pageItemCount(1));
     }
 
     @Test
-    public void testItemCount() {
+    public void testPageItemCount_LastPageWithCompleteNumberOfItems() {
+        PaginationHelper helper = new PaginationHelper(testValues, 3);
+        Assert.assertEquals("There should be 3 items on page 1 for 6-item array broken into pages with 3 items",
+                3, helper.pageItemCount(1));
     }
 
     @Test
-    public void testPageItemCount() {
+    public void testPageItemCount_PageIndexOutOfRange() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("There should be no page with index 2 for 6-item array broken into pages with 4 items",
+                -1, helper.pageItemCount(2));
+    }
+
+    // Tests for int pageIndex(int itemIndex) API
+
+    @Test
+    public void testPageIndex_FirstPageFirstItem() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("Item with index 0 should be on page with idx 0",
+                0, helper.pageIndex(0));
     }
 
     @Test
-    public void testPageIndex() {
+    public void testPageIndex_FirstPageLastItem() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("Item with index 3 should be on page with idx 0 for 6 item array broken to 4 items per page",
+                0, helper.pageIndex(3));
     }
 
     @Test
-    public void testGetItemsForPageIndex() {
+    public void testPageIndex_SecondPageFirstItem() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("Item with index 4 should be on page with idx 1 for 6 item array broken to 4 items per page",
+                1, helper.pageIndex(4));
     }
+
+    @Test
+    public void testPageIndex_LastPageLastItemForIncompleteLastPage() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("Item with index 5 should be on page with idx 1 for 6 item array broken to 4 items per page",
+                1, helper.pageIndex(5));
+    }
+
+    @Test
+    public void testPageIndex_LastPageLastItemForCompleteLastPage() {
+        PaginationHelper helper = new PaginationHelper(testValues, 3);
+        Assert.assertEquals("Item with index 5 should be on page with idx 1 for 6 item array broken to 3 items per page",
+                1, helper.pageIndex(5));
+    }
+
+    @Test
+    public void testPageIndex_IndexOutOfRange() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("There should be no page for item with index 6 for 6-item array broken into pages with 4 items",
+                -1, helper.pageIndex(6));
+    }
+
+    @Test
+    public void testPageIndex_NegativeValueIndex() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertEquals("There should be no page for negative index argument and -1 should be returned",
+                -1, helper.pageIndex(-2));
+    }
+
+    // Tests for Object[] getItemsForPageIndex(int pageIndex) API
+
+    @Test
+    public void testGetItemsForPageIndex_FirstPage() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertArrayEquals("Items {'a', 'b', 'c', 'd'} should be on the first page for defined array of 6 items broken into pages with 4 items count",
+                new Object[]{'a', 'b', 'c', 'd'}, helper.getItemsForPageIndex(0));
+    }
+
+    @Test
+    public void testGetItemsForPageIndex_LastPageForIncompleteLastPage() {
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        Assert.assertArrayEquals("Items {'e', 'f'} should be on the second page for defined array of 6 items broken into pages with 4 items count",
+                new Object[]{'e', 'f'}, helper.getItemsForPageIndex(1));
+    }
+
+    @Test
+    public void testGetItemsForPageIndex_LastPageForCompleteLastPage() {
+        PaginationHelper helper = new PaginationHelper(testValues, 3);
+        Assert.assertArrayEquals("Items {'d', 'e', 'f'} should be on the second page for defined array of 6 items broken into pages with 3 items count",
+                new Object[]{'d', 'e', 'f'}, helper.getItemsForPageIndex(1));
+    }
+
+    @Test
+    public void testGetItemsForPageIndex_IndexOutOfRange() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Page doesn't exist");
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        helper.getItemsForPageIndex(2);
+    }
+
+    @Test
+    public void testGetItemsForPageIndex_NegativeValueIndex() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Page doesn't exist");
+        PaginationHelper helper = new PaginationHelper(testValues, 4);
+        helper.getItemsForPageIndex(2);
+    }
+
 }
